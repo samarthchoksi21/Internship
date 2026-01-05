9090b914b68d186d2e1655ad3d92627eb2291879
Samarth Choksi <choksisamarth2@gmail.com>
2026-01-05 09:08:54 +0530

    Added seedroles and  added roles

--- Login/Backend/seed/seedRoles.js
+++ Login/Backend/seed/seedRoles.js
@@ -1,9 +1,31 @@
-const mongoose = require('mongoose')
+const path = require('path');
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env")
+});
 
+const mongoose = require("mongoose");
+const { ROLE } = require("../Models/roles");
+async function seedRoles(){
+    try {
+        await mongoose.connect(process.env.MONGO_URI).then(()=>{
+            console.log("CONNECTION STARTED")
+        })
 
-try {
-    
-    
-} catch (error) {
-    
+        const roles = ["ADMIN", "MANAGER" , "USER"]
+
+        for(const Rolename of roles){
+            const exist = await ROLE.findOne({name : Rolename})
+            if(!exist){
+                await ROLE.create({name : Rolename})
+                console.log(`Roles ${Rolename} created successfully`)
+            }
+        }
+    } catch (error) {
+        console.log("ERROR :",error)
+    }finally{
+        await mongoose.disconnect()
+        console.log("MONGODB DISCONNECTED")
+        process.exit(0)
+    }
 }
+seedRoles()
