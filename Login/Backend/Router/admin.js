bf8cdf0f6dc04afeadd7a4d78fa8568a665bd753
Samarth Choksi <choksisamarth2@gmail.com>
2026-01-05 22:27:55 +0530

    Made 2 APIs

--- Login/Backend/Router/admin.js
+++ Login/Backend/Router/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const Router = express.Router()
-const {ViewAllUser} = require('../controller/admin')
+const {ViewAllUser,CreateUser} = require('../controller/admin')
 const {VerifyUser} = require('../service/auth')
 const {checkAuth} = require('../middlewares/auth');
 Router.get('/allusers',VerifyUser,checkAuth("user:view"),ViewAllUser)
+Router.post('/createUser',VerifyUser,checkAuth("user:create"),CreateUser)
 module.exports = Router;
--- Login/Backend/controller/admin.js
+++ Login/Backend/controller/admin.js
@@ -1,4 +1,6 @@
 const {USER} = require('../Models/user')
+const {ROLE} = require('../Models/roles')
+const bcrypt = require('bcrypt')
 async function ViewAllUser(req,res){
     const users = await USER.find({},{
         password : 0,
@@ -13,7 +15,29 @@ async function ViewAllUser(req,res){
     })
 
 }
+async function CreateUser(req,res){
+    const {username,email,password} = req.body
+    if(!username || !email || !password){
+        return res.status(400).json({message : "Please enter credentials"})
+    }
+    const user = await USER.findOne({email})
+    if(user){
+        return res.status(400).json({message : "The user already exist"})
+    }
+    
+    const hashedpassword = await bcrypt.hash(password,10)
+    const userRole = await ROLE.findOne({name : "USER"})
+    await USER.create({
+        username,
+        email,
+        password : hashedpassword,
+        roleRef : userRole._id
+    })
+    
+    return res.status(200).json({message : "User created successfully"})
+}
 
 module.exports = {
-    ViewAllUser
+    ViewAllUser,
+    CreateUser
 }
