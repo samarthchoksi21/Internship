ea4369e2bdc8580ba08b93abc19de7a588a4e866
Samarth Choksi <choksisamarth2@gmail.com>
2026-01-23 22:19:41 +0530

    Finally done

--- IronGear/Backend/utils/mailer.js
+++ IronGear/Backend/utils/mailer.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendOtp(to, otp, text) {
   try {
     await resend.emails.send({
-      from: "IronGear <onboarding@resend.dev>", 
+      from: "IronGear <noreply@irongear.online>", 
       to,
       subject: "EMAIL VERIFICATION OTP",
       text: text || `Your OTP is ${otp}. Do not share this code.`,
